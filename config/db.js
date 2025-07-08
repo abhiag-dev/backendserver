26b3e436636d2fbf4aadf33ad8b4a421d1789bba
abhishek.ag <abhishek.ag@gmail.com>
2025-07-09 00:10:46 +0530

    env commit

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ let dbConnection;
 module.exports = {
   connectToDb: (cb) => {
     MongoClient.connect(
-      "mongodb+srv://abhishekag64064:7l8bmaaCTvD20gsh@cluster0.2iqr2uj.mongodb.net/vehicles",
+      "mongodb+srv://abhishekag64064:KNKYl07jQ3UmRJZp@cluster0.2iqr2uj.mongodb.net/vehicles",
       {
         serverSelectionTimeoutMS: 5000, // Example: Increase server selection timeout
         socketTimeoutMS: 30000, // Example: Increase socket timeout
--- config/express.js
+++ config/express.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
 const { getDb, connectToDb } = require("./db"); // Ensure this path is correct
